347fcc93988b5ea125f5b27d3764c17514cd2397
wudi <wicii2120@outlook.com>
2025-10-15 11:30:40 +0800

    edgy: don't handle help buffer

--- lua/plugins/edgy.lua
+++ lua/plugins/edgy.lua
@@ -1,21 +1,12 @@
 return {
-  "folke/edgy.nvim",
-  enabled = false,
-  ---@type Edgy.Config
-  opts = {
-    right = {
-      {
-        ft = "snacks_terminal",
-        size = { width = 0.3 },
-        title = "%{b:term_title}",
-
-        filter = function(_buf, win)
-          return vim.w[win].snacks_win
-            and vim.w[win].snacks_win.position == "right"
-            and vim.w[win].snacks_win.relative == "editor"
-            and not vim.w[win].trouble_preview
-        end,
-      },
-    },
-  },
+  'folke/edgy.nvim',
+  opts = function(_, opts)
+    ---@type Edgy.Pos
+    local bottom = opts.bottom
+    for i, v in ipairs(bottom) do
+      if type(v) == 'table' and v.ft == 'help' then
+        table.remove(bottom, i)
+      end
+    end
+  end,
 }
