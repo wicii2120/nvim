7533235d99a6d4a17403b105f84630cf6d03436f
wudi <wicii2120@outlook.com>
2025-10-15 16:54:07 +0800

    fix tailwindcss lsp bug

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -16,6 +16,41 @@ return {
           },
         },
       },
+      tailwindcss = {
+        settings = {
+          tailwindCSS = {
+            classFunctions = { 'twMerge', 'cva', 'tv' },
+            classAttributes = { 'class', 'className', 'ngClass', 'class:list', 'ui' },
+          },
+        },
+      },
+    },
+    setup = {
+      tailwindcss = function(_, opts)
+        opts.filetypes = opts.filetypes or {}
+
+        -- Add default filetypes
+        vim.list_extend(opts.filetypes, vim.lsp.config.tailwindcss.filetypes)
+
+        -- Remove excluded filetypes
+        --- @param ft string
+        opts.filetypes = vim.tbl_filter(function(ft)
+          return not vim.tbl_contains(opts.filetypes_exclude or {}, ft)
+        end, opts.filetypes)
+
+        -- Additional settings for Phoenix projects
+        opts.settings = vim.tbl_deep_extend('keep', opts.settings, {
+          tailwindCSS = {
+            includeLanguages = {
+              elixir = 'html-eex',
+              eelixir = 'html-eex',
+              heex = 'html-eex',
+            },
+          },
+        })
+        -- Add additional filetypes
+        vim.list_extend(opts.filetypes, opts.filetypes_include or {})
+      end,
     },
   },
 }
