09571c912dc2d03f62132e3908ecb1b697d21464
wicii2120 <wicii2120@outlook.com>
2025-12-14 00:11:57 +0800

    noice.nvim

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -15,6 +15,22 @@ return {
     },
   },
   opts = {
+    routes = {
+      {
+        filter = {
+          event = 'msg_show',
+          any = {
+            { find = '%d+L, %d+B' },
+            { find = '; after #%d+' },
+            { find = '; before #%d+' },
+          },
+        },
+        view = 'mini',
+      },
+    },
+    messages = {
+      enabled = true,
+    },
     lsp = {
       hover = {
         enabled = false,
@@ -35,11 +51,16 @@ return {
       -- you can enable a preset by setting it to true, or a table that will override the preset config
       -- you can also add custom presets that you can enable/disable with enabled=true
       bottom_search = false, -- use a classic bottom cmdline for search
-      command_palette = true, -- position the cmdline and popupmenu together
+      command_palette = false, -- position the cmdline and popupmenu together
       long_message_to_split = true, -- long messages will be sent to a split
       inc_rename = false, -- enables an input dialog for inc-rename.nvim
-      lsp_doc_border = true, -- add a border to hover docs and signature help
+      lsp_doc_border = false, -- add a border to hover docs and signature help
+      cmdline_output_to_split = true,
     },
+    ---@type NoiceConfigViews
+    views = {},
+    ---@type table<string, NoiceCommand>
+    commands = {},
   },
   -- stylua: ignore
   keys = {
@@ -51,29 +72,6 @@ return {
       mode = 'c',
       desc = 'Redirect Cmdline',
     },
-    {
-      '<c-f>',
-      function()
-        if not require('noice.lsp').scroll(4) then
-          return '<c-f>'
-        end
-      end,
-      mode = { 'n', 'i', 's' },
-      silent = true,
-      expr = true,
-    },
-    {
-      '<c-b>',
-      function()
-        if not require('noice.lsp').scroll(-4) then
-          return '<c-b>'
-        end
-      end,
-      mode = { 'n', 'i', 's' },
-      silent = true,
-      expr = true,
-    },
-    {'<leader>n', function() require('noice').cmd('pick') end, desc='Noice Picker'},
     {'<leader>snn', function() require('noice').cmd('pick') end, desc='Noice Picker'},
     {'<leader>snl', function() require('noice').cmd('last') end, desc='Noice Last'},
     {'<leader>snh', function() require('noice').cmd('history') end, desc='Noice History'},
