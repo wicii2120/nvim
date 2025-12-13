1253f7240ea5abcdf03a88d50651bff272bbc9e0
wicii2120 <wicii2120@outlook.com>
2025-12-13 20:10:53 +0800

    sidekick snacks picker integration

--- lua/plugins/sidekick.lua
+++ lua/plugins/sidekick.lua
@@ -15,6 +15,30 @@ return {
       },
     },
   },
+  specs = {
+    {
+      'snacks.nvim',
+      opts = {
+        picker = {
+          actions = {
+            sidekick_send = function(...)
+              return require('sidekick.cli.picker.snacks').send(...)
+            end,
+          },
+          win = {
+            input = {
+              keys = {
+                ['<a-a>'] = {
+                  'sidekick_send',
+                  mode = { 'n', 'i' },
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  },
   ---@type LazyKeysSpec[]
   keys = {
     {
