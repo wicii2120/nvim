a8de6344c511f86a7d3a0473d55f340e045697db
wicii2120 <wicii2120@outlook.com>
2025-12-13 20:27:10 +0800

    lsp symbols

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -6,21 +6,18 @@ return {
     vim.diagnostic.config({
       signs = {
         text = {
-          [vim.diagnostic.severity.ERROR] = ' ',
-          [vim.diagnostic.severity.WARN] = ' ',
-          [vim.diagnostic.severity.INFO] = '',
+          [vim.diagnostic.severity.ERROR] = '',
+          [vim.diagnostic.severity.WARN] = '',
+          [vim.diagnostic.severity.INFO] = '',
           [vim.diagnostic.severity.HINT] = '',
         },
       },
       virtual_text = {
         spacing = 4,
         source = 'if_many',
-        prefix = '●',
+        prefix = '',
         -- this will set set the prefix to a function that returns the diagnostics icon based on the severity
         -- prefix = "icons",
-        severity = {
-          min = vim.diagnostic.severity.WARN,
-        },
       },
       severity_sort = true,
     })
