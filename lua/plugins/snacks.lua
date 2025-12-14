c15a71410820825fdeff4c589a13adbae9eb7b9c
wicii2120 <wicii2120@outlook.com>
2025-12-14 16:43:55 +0800

    snacks

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -13,6 +13,7 @@ return {
           explorer = {
             hidden = true,
           },
+          git_diff = {},
         },
         previewers = {
           diff = {
@@ -76,6 +77,9 @@ return {
         },
       },
       bigfile = { enabled = true },
+      quickfile = { enabled = true },
+      input = { enabled = true },
+      debug = { enabled = true },
     },
 
     -- stylua: ignore
@@ -95,13 +99,13 @@ return {
       { '<leader>fp', function() Snacks.picker.projects() end, desc = 'Projects' },
       { '<leader>fr', function() Snacks.picker.recent() end, desc = 'Recent' },
       -- git
-      { '<leader>gb', function() Snacks.picker.git_branches() end, desc = 'Git Branches' },
       { '<leader>gl', function() Snacks.picker.git_log() end, desc = 'Git Log' },
       { '<leader>gL', function() Snacks.picker.git_log_line() end, desc = 'Git Log Line' },
       { '<leader>gs', function() Snacks.picker.git_status() end, desc = 'Git Status' },
       { '<leader>gS', function() Snacks.picker.git_stash() end, desc = 'Git Stash' },
       { '<leader>gd', function() Snacks.picker.git_diff() end, desc = 'Git Diff (Hunks)' },
       { '<leader>gf', function() Snacks.picker.git_log_file() end, desc = 'Git Log File' },
+      { '<leader>gb', function() Snacks.git.blame_line() end, desc = 'Git Blame Line' },
       -- gh
       { '<leader>gi', function() Snacks.picker.gh_issue() end, desc = 'GitHub Issues (open)' },
       { '<leader>gI', function() Snacks.picker.gh_issue({ state = 'all' }) end, desc = 'GitHub Issues (all)' },
