00e6a0b6fe671d6dae18103f387668964836f98b
wicii2120 <wicii2120@outlook.com>
2025-12-19 13:10:15 +0800

    vue_ls

---
+++ lsp/vue_ls.lua
@@ -0,0 +1,20 @@
+return {
+  settings = {
+    format = {
+      script = {
+        initialIndent = true,
+      },
+      style = {
+        initialIndent = true,
+      },
+    },
+    inlayHints = {
+      inlineHandlerLeading = true,
+      missingProps = true,
+    },
+    suggest = {
+      componentNameCasing = 'alwaysPascalCase',
+      propNameCasing = 'alwaysCamelCase',
+    },
+  },
+}
