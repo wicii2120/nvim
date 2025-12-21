78ca12d345d816533fe48fec4f254ca6755e339b
wicii2120 <wicii2120@outlook.com>
2025-12-22 02:05:22 +0800

    add sidekick statusline

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,9 +2,10 @@ return {
   {
     'nvim-lualine/lualine.nvim',
     enabled = true,
-    dependencies = { 'folke/noice.nvim' },
+    dependencies = { 'folke/noice.nvim', 'folke/sidekick.nvim' },
     opts = function()
       local noice = require('noice')
+      local sidekick = require('sidekick.status')
 
       return {
         options = {
@@ -56,9 +57,60 @@ return {
             'diagnostics',
           },
           lualine_c = {
+            -- showcmd
             {
               noice.api.status.command.get_hl,
               cond = noice.api.status.command.has,
+            },
+            -- Copilot LSP
+            {
+              function()
+                local status = sidekick.get()
+                if not status then
+                  return nil
+                end
+                if status.kind == 'Error' then
+                  return ''
+                end
+                if status.kind == 'Warning' then
+                  return ''
+                end
+                if status.kind == 'Normal' or status.kind == 'Inactive' then
+                  return ''
+                end
+              end,
+              color = function()
+                local status = sidekick.get()
+                if not status then
+                  return nil
+                end
+                if status.kind == 'Error' then
+                  return 'DiagnosticError'
+                end
+                if status.kind == 'Warning' then
+                  return 'DiagnosticWarn'
+                end
+                if status.busy then
+                  return 'DiagnosticWarn'
+                end
+                return 'Special'
+              end,
+              cond = function()
+                return sidekick.get() ~= nil
+              end,
+            },
+            -- CLI Session
+            {
+              function()
+                local status = sidekick.cli()
+                return '󱙺 ' .. (#status > 1 and #status or '')
+              end,
+              cond = function()
+                return #sidekick.cli() > 0
+              end,
+              color = function()
+                return 'Special'
+              end,
             },
           },
           lualine_x = {
