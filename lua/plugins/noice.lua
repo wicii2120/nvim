d720843ae90f3c306b34b82e447e33499e34ee3d
wicii2120 <wicii2120@outlook.com>
2026-01-07 15:30:46 +0800

    disable inc-rename
    breaks in vue sfc

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -27,7 +27,7 @@ return {
       bottom_search = false,         -- use a classic bottom cmdline for search
       command_palette = false,       -- position the cmdline and popupmenu together
       long_message_to_split = false, -- long messages will be sent to a split
-      inc_rename = true,             -- enables an input dialog for inc-rename.nvim
+      inc_rename = false,            -- enables an input dialog for inc-rename.nvim
       lsp_doc_border = false,        -- add a border to hover docs and signature help
       cmdline_output_to_split = false,
     },
